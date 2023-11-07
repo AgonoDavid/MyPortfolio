9443a038ccbfc87799a6b45209cbc62948a55b71
AgonoDavid <davidagono239@gmail.com>
2023-11-07 17:14:22 +0100

    updated my projects

--- src/components/ProjectsConfig.js
+++ src/components/ProjectsConfig.js
@@ -1,26 +1,13 @@
-// import Emojigit from "../images/Emojigit.PNG";
-// import HuluClone from "../images/HuluClone.PNG";
-// import KryptPreview from "../images/KryptPreview.jpg";
-// import FormVibePreview from "../images/FormVibePreview.png";
-// import DevBlogsPreview from "../images/DevBlogsPreview.PNG";
-// import CloudSpacePreview from "../images/CloudSpacePreview1.png";
-// import PortfolioPreview from "../images/preview.jpg";
-// import ChatSpacePreview from "../images/ChatSpacePreview.png";
-// import BlogBuddyPreview from "../images/BlogBuddyPreview.png";
+import DigitalAgency from "../images/DigitalAgency.png";
+import Rockets from "../images/RocketsWebsite.png";
+import SpaceWeb from "../images/SpaceWebsite.png";
+import Todo from "../images/TodoApp.png";
+import Landwind from "../images/Landwind.png";
 
 const projectsConfig = [
   {
-    title: "FormVibe",
-    // previewImage: FormVibePreview,
-    description:
-      "Form creation tool similar to typeform and form.app Built using nextjs. Supports typeform and Airtable type of forms. Supports drag and drop functionality to create forms with ease. User can share links of form to get responses",
-    liveDemoLink: "https://form-vibe.vercel.app/",
-    githubLink: "https://github.com/yatharth1706/FormVibe",
-    backgroundColor: "bg-blue-500",
-  },
-  {
-    title: "Portfolio",
-    // previewImage: PortfolioPreview,
+    title: "SPACE X",
+    previewImage: Rockets,
     description:
       "Portfolio website built using ReactJS and TailwindCSS. Includes information about me and my skills along with side projects.",
     liveDemoLink: "https://yatharth-portfolio.vercel.app/",
@@ -28,17 +15,8 @@ const projectsConfig = [
     backgroundColor: "bg-blue-500",
   },
   {
-    title: "BlogBuddy",
-    // previewImage: BlogBuddyPreview,
-    description:
-      "Full stack blogging application which i actually built as a part of assignment for one startup hiring. It was fun building this project. Learnt a lot of things. Built using nextjs, tailwind css, mongodb and appwrite",
-    liveDemoLink: "https://blog-buddy-seven.vercel.app/",
-    githubLink: "https://github.com/yatharth1706/BlogBuddy",
-    backgroundColor: "",
-  },
-  {
-    title: "Emoji Git CLI Tool",
-    // previewImage: Emojigit,
+    title: "TODO App",
+    previewImage: Todo,
     description:
       "In this project, I built Interactive CLI Tool for writing emojis in commit messages.It gives user some list of emojis in terminal before making commit. From that list user can select emojis according to the commit task he has done. After choosing that user can right commit message along with that emoji.",
     liveDemoLink: "https://www.npmjs.com/package/emojigit",
@@ -47,55 +25,33 @@ const projectsConfig = [
       "https://img.shields.io/npm/dt/emojigit.svg?style=flat-square)](https://www.npmjs.com/package/emojigit",
     backgroundColor: "bg-indigo-500",
   },
+  {
+    title: "Digital Agency Landing Page",
+    previewImage: DigitalAgency,
+    description:
+      "Form creation tool similar to typeform and form.app Built using nextjs. Supports typeform and Airtable type of forms. Supports drag and drop functionality to create forms with ease. User can share links of form to get responses",
+    liveDemoLink: "https://form-vibe.vercel.app/",
+    githubLink: "https://github.com/yatharth1706/FormVibe",
+    backgroundColor: "bg-blue-500",
+  },
+  {
+    title: "Branding Landing Page",
+    previewImage: Landwind,
+    description:
+      "Full stack blogging application which i actually built as a part of assignment for one startup hiring. It was fun building this project. Learnt a lot of things. Built using nextjs, tailwind css, mongodb and appwrite",
+    liveDemoLink: "https://blog-buddy-seven.vercel.app/",
+    githubLink: "https://github.com/yatharth1706/BlogBuddy",
+    backgroundColor: "",
+  },
 
   {
-    title: "Chat Space",
-    // previewImage: ChatSpacePreview,
+    title: "Solar System",
+    previewImage: SpaceWeb,
     description:
       "Fast One to One messaging app built using NextJS, Styled Components, Firebase v8 (Firestore and Auth), and React Firebase hooks for realtime fetching of data from the database. It allows user to send messages in realtime and can see when the person was last time online. It was amazing learning experience while building this project. ",
     liveDemoLink: "https://chat-space.vercel.app",
     githubLink: "https://github.com/yatharth1706/chatspace",
     backgroundColor: "bg-gray-800",
-  },
-
-  {
-    title: "Krypt",
-    // previewImage: KryptPreview,
-    description:
-      "Send crypto across the world using Krypt. Built my first dApp. Learnt it from JavaScript Mastery. It was really exciting to build this. Learnt a lot about blockchain, solidity, how to write smart contracts and get them deployed on blockchain. It was amazing learning experience",
-    liveDemoLink: "https://github.com/yatharth1706/dApp-web3",
-    githubLink: "https://github.com/yatharth1706/dApp-web3",
-    backgroundColor: "bg-[#03032952]",
-  },
-
-  {
-    title: "CloudSpace",
-    // previewImage: CloudSpacePreview,
-    description:
-      "Efficient storage solution. Support image file upload right now. Provides download url as well as preview of uploaded file. Tech stack used in this are AWS Amplify, Cognito, API Gateway, DynamoDB and ReactJS",
-    liveDemoLink: "https://cloud-space.vercel.app/",
-    githubLink: "https://github.com/yatharth1706/Cloud-Space",
-    backgroundColor: "bg-yellow-500",
-  },
-
-  {
-    title: "Hulu Clone 2.0",
-    // previewImage: HuluClone,
-    description:
-      "Side project to refresh TailwindCSS and Next JS Skills. Used TMDB API for showing movies and their description and other meta information.",
-    liveDemoLink: "https://hulu-clone-iota.vercel.app/",
-    githubLink: "https://github.com/yatharth1706/hulu-clone",
-    backgroundColor: "bg-gray-800",
-  },
-
-  {
-    title: "DevBlogs",
-    // previewImage: DevBlogsPreview,
-    description:
-      "This website is basically for sharing blogs with everyone. It gives us platform to write blogs in markdown language. Provides the preview also where we can see the final output of the blog post. It was kind of like a side project to refresh my frontend skills as well as backend skills. Tech stack using in this are React and Firebase",
-    liveDemoLink: "https://dev-blogs-blond.vercel.app/",
-    githubLink: "https://github.com/yatharth1706/DevBlogs",
-    backgroundColor: "bg-green-500",
   },
 ];
 
